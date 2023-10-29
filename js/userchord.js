2ff59282fddd68596d02721471e1c45c5b271b54
guy nut <82942709+guynut@users.noreply.github.com>
2023-10-29 17:17:20 +0700

    🔨 userchord.js

--- js/userchord.js
+++ js/userchord.js
@@ -13,3 +13,12 @@ crChord.addEventListener('click',() => {
     }
     else {}
 });
+
+// Get all elements with the "chord" class
+const chordElements = document.querySelectorAll('.chord');
+
+// Loop through the elements and set the background image based on the data attribute
+chordElements.forEach((element) => {
+    const backgroundImage = element.getAttribute('data-background-image');
+    element.style.backgroundImage = `linear-gradient(rgba(80, 71, 88, 0.267), #25243b), url(${backgroundImage})`;
+});
