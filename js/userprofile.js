e0adb81dc33c480ca91f225d2f8757f0b6ea12b7
guy nut <82942709+guynut@users.noreply.github.com>
2023-11-03 03:24:19 +0700

    🔨 userprofile.js

--- js/userprofile.js
+++ js/userprofile.js
@@ -115,3 +115,45 @@ chordcreate.addEventListener('click',() => {
     }
     else {}
 });
+
+// ================================================================
+// =================== Delete Pop-Up ======================
+// ================================================================
+// ================================================================
+document.addEventListener("DOMContentLoaded", function () {
+    // Add click event listener to all comment buttons
+    const delButtons = document.querySelectorAll(".delete-show");
+
+    delButtons.forEach(delButton => {
+        delButton.addEventListener("click", function (event) {
+            // Get the target post ID from the data attribute
+            const postID = event.currentTarget.dataset.target;
+            const delFill = document.querySelector(".delete-fill");
+
+            // Display the corresponding comment section
+            delFill.style.display = "flex";
+        });
+    });
+
+    // Add click event listener to close comment sections
+    const closedelFills = document.querySelectorAll(".btn-delete");
+
+    closedelFills.forEach(closedelFill => {
+        closedelFill.addEventListener("click", function (event) {
+            let delFill = document.querySelector(".delete-fill");
+
+            delFill.style.display = "none";
+        });
+    });
+
+    // Add click event listener to close comment sections when clicking outside
+    const deleteFills = document.querySelectorAll(".delete-fill");
+
+    deleteFills.forEach(deleteFill => {
+        deleteFill.addEventListener("click", function (e) {
+            if (e.target === deleteFill) {
+                deleteFill.style.display = "none";
+            }
+        });
+    });
+});
