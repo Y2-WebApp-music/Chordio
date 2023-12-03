6e079480e13afc3dad5bb10f33768a05ed80aa85
DRepresser <24zjokerr16@gmail.com>
2023-12-03 17:08:00 +0700

    change pass

--- routes/edit-pass.js
+++ routes/edit-pass.js
@@ -2,28 +2,47 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const db = require('./db');
+const multer = require('multer');
+const upload = multer();
 
 
-router.post('/edit-pass', (req, res) => {
+router.post('/edit-pass', upload.none(), (req, res) => {
     const user = req.session.user;
     const { npassword, password, cpassword } = req.body;
 
     bcrypt.compare(npassword, user.password, (err, isMatch) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send({ error: 'Internal Server Error' });
+        }
+
         if(isMatch) {
             if(password === cpassword) {
-                const query = 'UPDATE users SET password = ? WHERE user_id = ?'
                 bcrypt.hash(password, 12, (err, hash) => {
-                    if (err) throw err;
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).send({ error: 'Internal Server Error' });
+                    }
 
+                    const query = 'UPDATE users SET password = ? WHERE user_id = ?'
                     const data = [hash, user.user_id];
 
                     db.query(query, data, err => {
-                        if (err) throw err;
+                        if (err) {
+                            console.error(err);
+                            return res.status(500).send({ error: 'Internal Server Error' });
+                        }
+
+                        req.session.user.password = hash;
                         
-                        res.redirect('/setting');
+                        res.status(200).send({ message: 'Password changed successfully' });
                     });
                 });
+            } else {
+                return res.status(400).send({ error: 'Passwords do not match' });
             }
+        } else {
+            return res.status(400).send({ error: 'Current password is incorrect' });
         }
     });
 });
